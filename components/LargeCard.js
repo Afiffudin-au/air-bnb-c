48aa64c0c2e7126b8191b5d57dd6c48d8a5fa74b
afiffudin-au <afiffudin077@gmail.com>
2021-08-10 22:38:02 +0700

    modify

--- components/LargeCard.js
+++ components/LargeCard.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 function LargeCard({ img, title, descripton, buttonText }) {
   return (
     <section className='relative py-16 cursor-pointer'>
-      <div className='relative h-96 min-w-[300px]'>
+      <div className='relative h-96 min-w-[300px] hover:scale-105 transform transition duration-300 ease-out'>
         <Image
           src={img}
           className='rounded-2xl'
@@ -10,10 +10,10 @@ function LargeCard({ img, title, descripton, buttonText }) {
           objectFit='cover'
         />
       </div>
-      <div className='absolute top-32 left-12'>
+      <div className='absolute top-32 left-12  hover:scale-110 transform transition duration-150 ease-out'>
         <h3 className='text-4xl mb-3 w-64'>{title}</h3>
         <p>{descripton}</p>
-        <button className='text-sm text-white bg-gray-900 px-4 py-2 rounded-lg mt-5'>
+        <button className='hover:shadow-xl active:scale-90 transition duration-150 text-sm text-white bg-gray-900 px-4 py-2 rounded-lg mt-5'>
           {buttonText}
         </button>
       </div>
