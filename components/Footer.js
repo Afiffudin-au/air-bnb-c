0fb1d4ce534bf2d94676f5327ea38da1c65c23b4
Afiffudin-au <afiffudin077@gmail.com>
2021-12-07 18:36:02 +0700

    update

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600'>
+    <div className='grid grid-cols-1 md:grid-cols-3 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600'>
       <div className='space-y-4 text-xs text-gray-800'>
         <h5 className='font-bold'>ABOUT</h5>
         <p>How Airbnb works</p>
@@ -15,24 +15,9 @@ function Footer() {
         <p>This is not a real site</p>
         <p>Its a pretty awesome clone</p>
         <p>Referrals accepted</p>
-        <p>Papafam</p>
+        <p>Referrals accepted</p>
       </div>
-      <div className='space-y-4 text-xs text-gray-800'>
-        <h5 className='font-bold'>HOST</h5>
-        <p>Papa React</p>
-        <p>Presents</p>
-        <p>Zero to Full Stack Heros </p>
-        <p>Hundreds of Students</p>
-        <p>Join Now</p>
-      </div>
-      <div className='space-y-4 text-xs text-gray-800'>
-        <h5 className='font-bold'>HOST</h5>
-        <p>Papa React</p>
-        <p>Presents </p>
-        <p>Zero Stack Hero</p>
-        <p>Hundreds of Students</p>
-        <p>Join Now</p>
-      </div>
+
       <div className='space-y-4 text-xs text-gray-800'>
         <h5 className='font-bold'>SUPPORT</h5>
         <p>Help Center</p>
